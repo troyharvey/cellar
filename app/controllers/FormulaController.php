c5ea578bfa75f83dc081d9d32e568f052b594cb5
Troy Harvey <tharvey@indatus.com>
2014-02-18 21:19:46 -0500

    Updated FormulaController@store to create and save user/formulas

--- app/controllers/FormulaController.php
+++ app/controllers/FormulaController.php
@@ -4,32 +4,16 @@ class FormulaController extends BaseController
 {
     public function store()
     {
-        $email = Input::get('email');
-        $formulae = $this->parseFormulae(Input::get('formulae'));
+        $user = User::create([
+            'email' => Input::get('email')
+        ]);
 
-        dd($formulae);
-    }
+        // Array of new Formulas from brew list string
+        $formulae = Formula::createFromBrewList(Input::get('formulae'));
 
-    private function parseFormulae($formulae)
-    {
-        // string 'autoconf 2.69\node 0.10.24 0.10.25'
-        // to
-        // array ['autoconf 2.69', 'node 0.10.24 0.10.25']
-        $formulae = explode("\n", $formulae);
-
-        foreach ($formulae as &$formula) {
-
-            // string 'node 0.10.24 0.10.25'
-            // to
-            // array ['node','0.10.24','0.10.25']
-            $f = explode(' ', $formula);
-
-            $formula = [
-                'formula' => array_shift($f), // 1st
-                'version' => array_pop($f)    // nth
-            ];
+        // Attach each formula
+        foreach ($formulae as $formula) {
+            $user->formulas()->attach($formulae);
         }
-
-        return $formulae;
     }
 }
--- app/models/Formula.php
+++ app/models/Formula.php
@@ -8,4 +8,37 @@ class Formula extends Eloquent
     {
         return $this->belongsToMany('User', 'user_formula');
     }
+
+    /**
+     * Create multiple Formulas from a brew list string
+     * @param  string $brew_list The raw dump of the brew list command
+     * @return array             Array of new Formula objects
+     */
+    public static function createFromBrewList($brew_list)
+    {
+        // string 'autoconf 2.69\node 0.10.24 0.10.25'
+        // to
+        // array ['autoconf 2.69', 'node 0.10.24 0.10.25']
+        $brew_list = explode("\n", $brew_list);
+
+        // Remove empty array elements caused by \n
+        $brew_list = array_filter($brew_list);
+
+        $formulae = [];
+
+        foreach ($brew_list as $formula) {
+
+            // string 'node 0.10.24 0.10.25'
+            // to
+            // array ['node','0.10.24','0.10.25']
+            $formula = explode(' ', $formula);
+
+            $formulae[] = Formula::create([
+                'name' => array_shift($formula), // 1st
+                'version' => array_pop($formula) // nth
+            ]);
+        }
+
+        return $formulae;
+    }
 }
